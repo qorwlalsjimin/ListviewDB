78bca119123c1839ca650159ea5c704988a2c030
지민 백 <qorwlalsjimin@naver.com>
2022-07-29 15:39:55 +0900

    insert 성공&실패 알려주는 Toast

--- app/src/main/java/kr/hs/s2104/listviewdb/MainActivity.java
+++ app/src/main/java/kr/hs/s2104/listviewdb/MainActivity.java
@@ -3,12 +3,38 @@ package kr.hs.s2104.listviewdb;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    DatabaseHelper db;
+
+    Button add_data;
+    EditText add_name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        db = new DatabaseHelper(this);
+        add_data = findViewById(R.id.add_data);
+        add_name = findViewById(R.id.add_name);
+
+        add_data.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = add_name.getText().toString();
+                if(!name.equals("") && db.insertData(name)) {
+                    Toast.makeText(MainActivity.this, "Data added", Toast.LENGTH_SHORT).show();
+                    add_name.setText("");
+                }else{
+                    Toast.makeText(MainActivity.this, "Data not added", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }

f160a36acb086eacfaa34381ed413232986bf506
지민 백 <qorwlalsjimin@naver.com>
2022-07-29 15:07:32 +0900

    미림 운세를 위한 DB 연동 연습 예제

---
+++ app/src/main/java/kr/hs/s2104/listviewdb/MainActivity.java
@@ -0,0 +1,14 @@
+package kr.hs.s2104.listviewdb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
